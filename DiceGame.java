386136de5a0aee03feb5b55270d6cd9dee6b66d6
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-12 17:12:58 -0500

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -1,3 +1,22 @@
 //Scan all primitive data types
 import java.util.Random;
 import java.util.Scanner;
+
+public class DiceGame {
+ 
+  public static void main(String[] args) {
+
+    //Create a new random function
+    Random roll = new Random();
+
+    // This will give a range from 0 - 5
+    int randNum = roll.nextInt(6);
+    
+    //Declare two integer variables
+    int counter;
+    
+    // Declare all nescessary variables 
+    //I need to have one so that within the range it will no longer be from 0-5 but 1-6
+    randNum += 1;
+    counter = 0;
+    
