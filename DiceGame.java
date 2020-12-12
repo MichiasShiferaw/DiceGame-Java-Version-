02368bc8d22116d68e133eebf35afdfc1e0df0b0
TheShiferawShow <68077955+TheShiferawShow@users.noreply.github.com>
2020-12-12 17:13:17 -0500

    Update DiceGame.java

--- DiceGame.java
+++ DiceGame.java
@@ -20,3 +20,19 @@ public class DiceGame {
     randNum += 1;
     counter = 0;
     
+    int guess;
+    Scanner userInput = new Scanner(System.in);
+    // Asks the user what the random number is
+    System.out.println("I got a number that you got to try and guess it is between 1 & 6. ");
+    guess = userInput.nextInt();
+
+    // Repeats this process as long as the answer is wrong
+    while (randNum != guess) {
+
+      // One more trial that you took is added to the total
+      counter += 1;
+
+      //Inform the user that the guessed number was not equivalent to the hidden number       
+      System.out.print("That number is incorrect! ");
+
+
